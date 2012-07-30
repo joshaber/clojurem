5949ba6a5e02afc299be721405a788913bfda174
joshaber <joshaber@gmail.com>
2012-07-29 22:32:03 -0400

    use dispatch_queue specificity instead of thread

--- runtime/CLJMRuntime/CLJMRuntime/CLJMRT.m
+++ runtime/CLJMRuntime/CLJMRuntime/CLJMRT.m
@@ -6,9 +6,10 @@
 //  Copyright (c) 2012 Josh Abernathy. All rights reserved.
 //
 
-#import <pthread.h>
 #import "CLJMRT.h"
 #import "CLJMVar.h"
+
+static const void *CLJMRuntimeBindingsKey = &CLJMRuntimeBindingsKey;
 
 static dispatch_queue_t CLJMRuntimeKeywordsQueue;
 static dispatch_queue_t CLJMRuntimeSymbolsQueue;
@@ -17,8 +18,6 @@ static dispatch_queue_t CLJMRuntimeDefsQueue;
 static NSMutableDictionary *CLJMRuntimeKeywords = nil;
 static NSMutableDictionary *CLJMRuntimeSymbols = nil;
 static NSMutableDictionary *CLJMRuntimeDefs = nil;
-
-static pthread_key_t CLJMRuntimeBindingsKey = 0;
 
 __attribute__((constructor))
 static void cljm_rt_init(void) {
@@ -29,8 +28,6 @@ static void cljm_rt_init(void) {
 	CLJMRuntimeKeywords = [[NSMutableDictionary alloc] init];
 	CLJMRuntimeSymbols = [[NSMutableDictionary alloc] init];
 	CLJMRuntimeDefs = [[NSMutableDictionary alloc] init];
-	
-	pthread_key_create(&CLJMRuntimeBindingsKey, NULL);
 }
 
 __attribute__((destructor))
@@ -38,8 +35,6 @@ static void cljm_rt_teardown(void) {
 	dispatch_release(CLJMRuntimeKeywordsQueue);
 	dispatch_release(CLJMRuntimeSymbolsQueue);
 	dispatch_release(CLJMRuntimeDefsQueue);
-	
-	pthread_key_delete(CLJMRuntimeBindingsKey);
 }
 
 BOOL cljm_truthy(id object) {
@@ -87,13 +82,13 @@ CLJMVar * cljm_var_def(NSString *name, id<NSCopying> value) {
 }
 
 NSMutableDictionary * cljm_rt_get_top_bindings(void) {
-	NSMutableArray *bindingsStack = (__bridge NSMutableArray *) pthread_getspecific(CLJMRuntimeBindingsKey);
+	NSMutableArray *bindingsStack = (__bridge NSMutableArray *) dispatch_queue_get_specific(dispatch_get_current_queue(), CLJMRuntimeBindingsKey);
 	NSMutableDictionary *topBindings = [bindingsStack lastObject];
 	return topBindings;
 }
 
 CLJMVar * cljm_var_lookup(NSString *name) {
-	NSMutableArray *bindingsStack = (__bridge NSMutableArray *) pthread_getspecific(CLJMRuntimeBindingsKey);
+	NSMutableArray *bindingsStack = (__bridge NSMutableArray *) dispatch_queue_get_specific(dispatch_get_current_queue(), CLJMRuntimeBindingsKey);
 	for(NSDictionary *bindings in [bindingsStack reverseObjectEnumerator]) {
 		id boundValue = [bindings objectForKey:name];
 		if(boundValue != nil) return boundValue;
@@ -114,16 +109,16 @@ CLJMVar * cljm_var_bind(NSString *name, id<NSCopying> value) {
 }
 
 void cljm_push_binding(void) {
-	NSMutableArray *bindingsStack = (__bridge NSMutableArray *) pthread_getspecific(CLJMRuntimeBindingsKey);
+	NSMutableArray *bindingsStack = (__bridge NSMutableArray *) dispatch_queue_get_specific(dispatch_get_current_queue(), CLJMRuntimeBindingsKey);
 	if(bindingsStack == nil) {
 		bindingsStack = [NSMutableArray array];
-		pthread_setspecific(CLJMRuntimeBindingsKey, (__bridge const void *) bindingsStack);
+		dispatch_queue_set_specific(dispatch_get_current_queue(), CLJMRuntimeBindingsKey, (__bridge void *) bindingsStack, NULL);
 	}
 	
 	[bindingsStack addObject:[NSMutableDictionary dictionary]];
 }
 
 void cljm_pop_binding(void) {
-	NSMutableArray *bindingsStack = (__bridge NSMutableArray *) pthread_getspecific(CLJMRuntimeBindingsKey);
+	NSMutableArray *bindingsStack = (__bridge NSMutableArray *) dispatch_queue_get_specific(dispatch_get_current_queue(), CLJMRuntimeBindingsKey);
 	[bindingsStack removeLastObject];
 }
